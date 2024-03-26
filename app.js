39ccefd6161e465e5f1a8bed22e32609e388de7e
Rick Sanchez <jmiqueiasns1302@gmail.com>
2024-03-26 16:50:54 -0300

    mind-granete file added.

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs')
+const util = require('util')
 
-const getText = (path) => {
-    return new Promise((resolve, reject)=>{
-        readFile(path, 'utf8', (err, result)=>{
-            if(err){
-                reject(err);
-            }
-            else{
-                resolve(result);
-            }
-        })
-    })
-}
-
-// getText('./content/first.txt').then((data => {console.log(data)})).catch(err => {console.log(err)})
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async ()=>{
     try {
-        const first = await getText('./content/first.txt');
-        console.log(first)
+        const first = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        await writeFilePromise('./content/result-mind-granate.txt', 'These shall be the blueprints for the project.')
+        console.log(first, second)
     } catch (error) {
         console.log(error)
     }
 }
 
 start()
+console.log('last.')
+
+
+
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8', (err, result)=>{
+//             if(err){
+//                 reject(err);
+//             }
+//             else{
+//                 resolve(result);
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt').then((data => {console.log(data)})).catch(err => {console.log(err)})
